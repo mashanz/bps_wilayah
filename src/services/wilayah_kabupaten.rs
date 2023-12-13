7bf875f57ab58f6b2e6162788f671e1782450971
Hanjara <hanjara@impstudio.co.id>
2023-12-13 14:32:38 +0700

    feat: read paths

--- src/services/wilayah_kabupaten.rs
+++ src/services/wilayah_kabupaten.rs
@@ -1,5 +1,6 @@
-use actix_web::{HttpResponse, Responder};
+use actix_web::{web, HttpResponse, Responder};
 
-pub async fn service() -> impl Responder {
+pub async fn service(path: web::Path<(String, String, String)>) -> impl Responder {
+    println!("{:?}", path);
     HttpResponse::Ok().body("Hey there!")
 }

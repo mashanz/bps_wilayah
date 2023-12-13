c4e28b51d6590b386035b92245d3d21a3f6e4a25
Hanjara <hanjara@impstudio.co.id>
2023-12-13 15:25:05 +0700

    fix: read csv to memory

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,20 @@ pub mod services;
 use crate::app_data::AppData;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use polars::prelude::*;
+use std::sync::Arc;
+
+fn read_data() -> PolarsResult<DataFrame> {
+    let mut schema = Schema::new();
+    schema.with_column("kode_bps".into(), DataType::Categorical(None));
+    schema.with_column("nama_bps".into(), DataType::Categorical(None));
+    schema.with_column("kode_dagri".into(), DataType::Categorical(None));
+    schema.with_column("nama_dagri".into(), DataType::Categorical(None));
+
+    CsvReader::from_path("./data.csv")?
+        .has_header(true)
+        .with_dtypes(Some(Arc::new(schema)))
+        .finish()
+}
 
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
@@ -12,10 +26,10 @@ async fn manual_hello() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        let df = read_data().unwrap();
+        println!("{:?}", df);
         App::new()
-            .app_data(web::Data::new(AppData {
-                data: DataFrame::default(),
-            }))
+            .app_data(web::Data::new(AppData { data: df }))
             .route("/hey", web::get().to(manual_hello))
             .service(services::service())
     })
